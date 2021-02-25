5b363e3f48b49039ceadee5ac9ccf4d8cf7e3446
mtrimner <matt.trimner@gmail.com>
2021-02-25 09:07:09 -0800

    finished user authentication with bcrypt and jwt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -18,7 +19,33 @@ userSchema.pre("save", function (next) {
     return next();
   }
 
-  bcrypt.genSalt(10, (err, salt) => {});
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
 });
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  const user = this;
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      } else {
+        return resolve(true);
+      }
+    });
+  });
+};
 
 mongoose.model("User", userSchema);
