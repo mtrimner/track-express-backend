a6c0153f4e2c418c583c676af5b6f4681bff2ca0
mtrimner <matt.trimner@gmail.com>
2021-02-27 15:46:12 -0800

    finished creating routes for track app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require("./models/User");
+require("./models/Track");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = "mongodb+srv://mtrimner:%24tr3ngthcomes%23ONE@cluster0.9qife.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.connect(mongoUri, {
